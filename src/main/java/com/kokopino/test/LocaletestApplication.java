874a9f26b3453e99794e10e4b0ffdc962c497fae
Juha Kervinen <joker@iki.fi>
2017-04-04 11:38:27 +0300

    Initial commit from initializr

---
+++ src/main/java/com/kokopino/test/LocaletestApplication.java
@@ -0,0 +1,12 @@
+package com.kokopino.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaletestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaletestApplication.class, args);
+	}
+}
