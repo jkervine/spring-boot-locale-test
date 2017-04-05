3022ddf76408ea7e3af4162b0a33c8c6ca611c4e
Juha Kervinen <joker@iki.fi>
2017-04-05 07:06:02 +0300

    Try locale resolution through mocked mvc test

--- src/test/java/com/kokopino/test/LocaletestApplicationTests.java
+++ src/test/java/com/kokopino/test/LocaletestApplicationTests.java
@@ -1,16 +1,45 @@
 package com.kokopino.test;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LocaletestApplicationTests {
 
+	@Autowired
+	LocalePrinterService localePrinterService;
+
+	@Autowired
+	WebApplicationContext context;
+
+	MockMvc mockMvc;
+	MockHttpServletRequestBuilder mockRequestBuilder;
+
+	@Before
+	public void setUp() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		mockRequestBuilder = MockMvcRequestBuilders.get("/printlocale");
+	}
+
 	@Test
 	public void contextLoads() {
+	}
+
+	@Test
+	public void testLocaleChange() {
+		MockHttpServletRequest testRequest = mockRequestBuilder.header("Accept-Language", "fi-FI").buildRequest(context.getServletContext());
+		localePrinterService.printLocale(testRequest);
 	}
 
 }
